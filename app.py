7c99c40447c31ec927404d0c25d94eb0458ca0a9
mssharma659 <ms9193856853@gmail.com>
2026-04-08 22:47:06 +0530

    ready for deployment

--- app.py
+++ app.py
@@ -236,5 +236,5 @@ def api_data():
     return data
 
 # ----------------------
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
